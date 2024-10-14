a8f9f7068ab38978e7cbdb999a77c667b35c9843
buchion <buchionwuasoanya@hotmail.com>
2024-10-14 14:04:39 +0100

    Many Links Step 32, Increase Block by 90%

--- generate.py
+++ generate.py
@@ -11,6 +11,7 @@ width = 1
 height = 1
 
 for t in range(0, 10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y, z + (t*1)] , size=[length * (t*1), width, height])
-
+    pyrosim.Send_Cube(name="Box", pos=[x,y, z + (t*1)] , size=[size, size, size])
+    size = size * .9
+    
 pyrosim.End()
