9849701e71413dffc2d1a09486f70161b08805b6
buchion <buchionwuasoanya@hotmail.com>
2024-10-15 09:55:21 +0100

    Many Links Last Push

--- generate.py
+++ generate.py
@@ -10,8 +10,19 @@ length = 1
 width = 1
 height = 1
 
-for t in range(0, 10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y, z + (t*1)] , size=[size, size, size])
-    size = size * .9
-    
+for j in range(0, 5):
+    for i in range(0, 5):
+        for t in range(0, 10):
+            pyrosim.Send_Cube(name="Box", pos = [x, y, z + (t*1)] , size=[length, width, height])
+            length = length * .9
+            width = width * .9
+            height = height * .9
+
+        y = (y + 1)
+        length = 1
+        width = 1
+        height = 1
+    y = 0
+    x = x + 1
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -8,14 +8,13 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 # https://imgur.com/a/xjVefDL
 
-
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 for t in range(0, 1000):
     p.stepSimulation()
-    time.sleep(1/60)
+    # time.sleep(1/60)
     print(t)
 
 
